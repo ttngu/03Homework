db65054fe2e8010751f740cd1a5f7f41884c3466
ttngu <tammyn137@gmail.com>
2020-11-01 22:42:23 -0800

    cleaned up the character variables, removed the extra spaces

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // Characters
-var inputLowercase = [  "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",];
-var inputUppercase = [  "A",  "B",  "C",  "D",  "E", "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z",];
-var inputNumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var inputLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var inputUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var inputNumeric = ["0","1","2","3","4","5","6","7","8","9"];
 var inputSpecial = ["@","%","+","\\","/","'","!","#","$","^","?",":",",",")","(","}","{","]","[","~","-","_",".",];
 
 //Empty Arrays
